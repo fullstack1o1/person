db81deb35e2fe0e23d7772d220cca79047e2ee0d
samitkumarpatel <samitkumarpatel@gmail.com>
2024-11-26 16:14:19 +0000

    UI and bit of backend integration

--- src/main/java/net/samitkumar/person/PersonApplication.java
+++ src/main/java/net/samitkumar/person/PersonApplication.java
@@ -2,6 +2,11 @@ package net.samitkumar.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 @SpringBootApplication
 public class PersonApplication {
@@ -9,4 +14,17 @@ public class PersonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
 	}
+
+	@Bean
+	CorsWebFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOriginPattern("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsWebFilter(source);
+	}
+
 }
